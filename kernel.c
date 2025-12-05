49fb8c471b983a0cb09da20d701358d530ed3b35
rael-link <george.danforth@proton.me>
2025-12-05 12:31:41 -0600

    adds PANIC

--- kernel.c
+++ kernel.c
@@ -31,6 +31,9 @@ void putchar(char ch) {
 void kernel_main(void) {
 
 	memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
+
+	PANIC("booted!");
+	PANIC("unreachable here!\n");
 	
         printf("\n\nHello %s\n" , "World!");
 	printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
