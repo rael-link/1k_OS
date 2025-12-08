93b798500bb0038bd3c9de156cb15ad75fbad289
rael-link <george.danforth@proton.me>
2025-12-08 09:31:31 -0600

    adds paging tables

--- kernel.h
+++ kernel.h
@@ -5,6 +5,12 @@
 #define PROCS_MAX 8
 #define PROC_UNUSED 0
 #define PROC_RUNNABLE 1
+#define SATP_SV32 (1u << 31)
+#define PAGE_V    (1 << 0)     //"Valid" bit (entry is enabled)
+#define PAGE_R    (1 << 1)     //Readable
+#define PAGE_W    (1 << 2)     //Writable
+#define PAGE_X    (1 << 3)     //Executable
+#define PAGE_U    (1 << 4)     //User (accessible in user mode)
 
 #define PANIC(fmt, ...)                                                       \
     do {                                                                      \
@@ -30,6 +36,7 @@ struct process {
     int pid;               //Process ID
     int state;             //Process state: PROC_UNUSED or PROC_RUNNABLE
     vaddr_t sp;            //Stack pointer
+    uint32_t *page_table;
     uint8_t stack[8192];   //Kernel stack
 };
 
